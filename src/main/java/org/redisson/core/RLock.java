dadb140d054f6eb34f20306ab840bc059ec098c4
Nikita <abracham.mitchell@gmail.com>
2014-01-17 16:26:35 +0400

    RLock comments added

--- src/main/java/org/redisson/core/RLock.java
+++ src/main/java/org/redisson/core/RLock.java
@@ -20,18 +20,39 @@ import java.util.concurrent.locks.Lock;
 /**
  * Distributed implementation of {@link java.util.concurrent.locks.Lock}
  * Implements reentrant lock.
+ * Use {@link RLock#getHoldCount()} to get a holds count.
  *
  * @author Nikita Koksharov
  *
  */
 public interface RLock extends Lock, RObject {
 
+    /**
+     * Unlocks lock independently of state
+     *
+     */
     void forceUnlock();
 
+    /**
+     * Checks if this lock locked by any thread in Redisson cluster
+     *
+     * @return <code>true</code> if locked otherwise <code>false</code>
+     */
     boolean isLocked();
 
+    /**
+     * Checks if this lock is held by the current thread
+     *
+     * @return @return <code>true</code> if held by current thread
+     * otherwise <code>false</code>
+     */
     boolean isHeldByCurrentThread();
 
+    /**
+     * Number of holds on this lock by the current thread
+     *
+     * @return holds or <code>0</code> if this lock is not held by current thread
+     */
     int getHoldCount();
 
 }
