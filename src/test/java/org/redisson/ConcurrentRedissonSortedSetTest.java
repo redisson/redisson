ef57a3d34485d5dea8162891e9eed88eaaf9718f
Nikita <abracham.mitchell@gmail.com>
2014-06-07 20:52:39 +0400

    Negative score test added

--- src/test/java/org/redisson/ConcurrentRedissonSortedSetTest.java
+++ src/test/java/org/redisson/ConcurrentRedissonSortedSetTest.java
@@ -35,7 +35,38 @@ public class ConcurrentRedissonSortedSetTest extends BaseConcurrentTest {
         for (int i = 1; i < length+1; i++) {
             elements.add(i);
         }
-        MatcherAssert.assertThat(map, Matchers.contains(elements.toArray(new Integer[elements.size()])));
+        Integer[] p = elements.toArray(new Integer[elements.size()]);
+        MatcherAssert.assertThat(map, Matchers.contains(p));
+
+        map.clear();
+        r.shutdown();
+    }
+
+    @Test
+    public void testAddNegative_SingleInstance() throws InterruptedException {
+        final String name = "testAddNegative_SingleInstance";
+
+        Redisson r = Redisson.create();
+        RSortedSet<Integer> map = r.getSortedSet(name);
+        map.clear();
+
+        int length = 1000;
+        final AtomicInteger counter = new AtomicInteger();
+        testSingleInstanceConcurrency(length, new RedissonRunnable() {
+            @Override
+            public void run(Redisson redisson) {
+                RSortedSet<Integer> set = redisson.getSortedSet(name);
+                int c = counter.decrementAndGet();
+                Assert.assertTrue(set.add(c));
+            }
+        });
+
+        List<Integer> elements = new ArrayList<Integer>();
+        for (int i = -length; i < 0; i++) {
+            elements.add(i);
+        }
+        Integer[] p = elements.toArray(new Integer[elements.size()]);
+        MatcherAssert.assertThat(map, Matchers.contains(p));
 
         map.clear();
         r.shutdown();
