1ea954bfa5aa135a885f22fb0a05bbdb5208ad06
Nikita <abracham.mitchell@gmail.com>
2014-07-29 15:53:33 +0400

    Comments added

--- src/main/java/org/redisson/RedissonClient.java
+++ src/main/java/org/redisson/RedissonClient.java
@@ -18,27 +18,101 @@ package org.redisson;
 import org.redisson.core.*;
 
 public interface RedissonClient {
+
+    /**
+     * Returns object holder by name
+     *
+     * @param name of object
+     * @return
+     */
     <V> RBucket<V> getBucket(String name);
 
+    /**
+     * Returns HyperLogLog object
+     *
+     * @param name of object
+     * @return
+     */
     <V> RHyperLogLog<V> getHyperLogLog(String name);
 
+    /**
+     * Returns list instance by name.
+     *
+     * @param name of list
+     * @return
+     */
     <V> RList<V> getList(String name);
 
+    /**
+     * Returns map instance by name.
+     *
+     * @param name of map
+     * @return
+     */
     <K, V> RMap<K, V> getMap(String name);
 
+    /**
+     * Returns lock instance by name.
+     *
+     * @param name of lock
+     * @return
+     */
     RLock getLock(String name);
 
+    /**
+     * Returns set instance by name.
+     *
+     * @param name of set
+     * @return
+     */
     <V> RSet<V> getSet(String name);
 
+    /**
+     * Returns sorted set instance by name.
+     *
+     * @param name of sorted set
+     * @return
+     */
     <V> RSortedSet<V> getSortedSet(String name);
 
+    /**
+     * Returns topic instance by name.
+     *
+     * @param name of the distributed topic
+     * @return
+     */
     <M> RTopic<M> getTopic(String name);
 
+    /**
+     * Returns queue instance by name.
+     *
+     * @param name of queue
+     * @return
+     */
     <V> RQueue<V> getQueue(String name);
 
+    /**
+     * Returns deque instance by name.
+     *
+     * @param name of deque
+     * @return
+     */
     <V> RDeque<V> getDeque(String name);
 
+    /**
+     * Returns "atomic long" instance by name.
+     *
+     * @param name of the "atomic long"
+     * @return
+     */
     RAtomicLong getAtomicLong(String name);
 
+    /**
+     * Returns "count down latch" instance by name.
+     *
+     * @param name of the "count down latch"
+     * @return
+     */
     RCountDownLatch getCountDownLatch(String name);
+
 }
