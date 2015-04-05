8f967f130ac462834ae13d44d40b1bf4018ef124
Nikita <abracham.mitchell@gmail.com>
2015-04-05 18:14:51 +0300

    BlockingQueue.poll fixed

--- src/main/java/org/redisson/RedissonBlockingQueue.java
+++ src/main/java/org/redisson/RedissonBlockingQueue.java
@@ -65,7 +65,7 @@ public class RedissonBlockingQueue<V> extends RedissonQueue<V> implements RBlock
 
     @Override
     public V poll(final long timeout, final TimeUnit unit) throws InterruptedException {
-        return connectionManager.read(getName(), new SyncOperation<V, V>() {
+        return connectionManager.write(getName(), new SyncOperation<V, V>() {
             @Override
             public V execute(RedisConnection<Object, V> conn) {
                 return conn.blpop(unit.toSeconds(timeout), getName()).value;
