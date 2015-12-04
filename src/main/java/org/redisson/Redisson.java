9ce5c6a7886267888f36f9c7b17ca34cf727eac0
Nikita <abracham.mitchell@gmail.com>
2015-12-04 12:33:26 +0300

    Redisson.createReactive fixed

--- src/main/java/org/redisson/Redisson.java
+++ src/main/java/org/redisson/Redisson.java
@@ -122,13 +122,13 @@ public class Redisson implements RedissonClient {
      *
      * @return Redisson instance
      */
-    public static RedissonClient createReactive() {
+    public static RedissonReactiveClient createReactive() {
         Config config = new Config();
         config.useSingleServer().setAddress("127.0.0.1:6379");
 //        config.useMasterSlaveConnection().setMasterAddress("127.0.0.1:6379").addSlaveAddress("127.0.0.1:6389").addSlaveAddress("127.0.0.1:6399");
 //        config.useSentinelConnection().setMasterName("mymaster").addSentinelAddress("127.0.0.1:26389", "127.0.0.1:26379");
 //        config.useClusterServers().addNodeAddress("127.0.0.1:7000");
-        return create(config);
+        return createReactive(config);
     }
 
     /**
