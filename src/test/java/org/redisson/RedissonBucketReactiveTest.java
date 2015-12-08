d84b98627d1fb395cb5f9dcd33a7b0147f20ca31
Nikita <abracham.mitchell@gmail.com>
2015-12-08 11:34:54 +0300

    RedissonBucketReactiveTest fixed

--- src/test/java/org/redisson/RedissonBucketReactiveTest.java
+++ src/test/java/org/redisson/RedissonBucketReactiveTest.java
@@ -76,7 +76,7 @@ public class RedissonBucketReactiveTest extends BaseReactiveTest {
         sync(bucket.set(value));
         Assert.assertEquals(value, sync(bucket.get()));
 
-        Assert.assertTrue(sync(bucket.exists()));
+        Assert.assertTrue(sync(bucket.isExists()));
     }
 
     @Test
@@ -87,11 +87,11 @@ public class RedissonBucketReactiveTest extends BaseReactiveTest {
         sync(bucket.set(value));
         Assert.assertEquals(value, sync(bucket.get()));
 
-        Assert.assertTrue(sync(bucket.exists()));
+        Assert.assertTrue(sync(bucket.isExists()));
 
         bucket.delete();
 
-        Assert.assertFalse(sync(bucket.exists()));
+        Assert.assertFalse(sync(bucket.isExists()));
     }
 
     @Test
