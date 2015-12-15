2420cb6238b504a34a61b775aa1815b3e8622f4c
Nikita <abracham.mitchell@gmail.com>
2015-12-15 21:44:36 +0300

    comments added

--- src/main/java/org/redisson/connection/ConnectionListener.java
+++ src/main/java/org/redisson/connection/ConnectionListener.java
@@ -19,8 +19,21 @@ import java.net.InetSocketAddress;
 
 public interface ConnectionListener {
 
+    /**
+     * This method will be triggered when Redisson
+     * connects to Redis server.
+     *
+     * @param addr - Redis server network address
+     */
     void onConnect(InetSocketAddress addr);
 
+    /**
+     * This method will be triggered when Redisson
+     * discovers that Redis server connected before
+     * now in disconnected state.
+     *
+     * @param addr - Redis server network address
+     */
     void onDisconnect(InetSocketAddress addr);
 
 }
