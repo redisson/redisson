e061d1c7d7d7ecdada20040fc6e87968fe4fb608
Nikita <abracham.mitchell@gmail.com>
2016-01-30 13:44:28 +0300

    Redisson failed to start when one of sentinel servers is down. Fixed.

--- src/main/java/org/redisson/connection/SentinelConnectionManager.java
+++ src/main/java/org/redisson/connection/SentinelConnectionManager.java
@@ -127,7 +127,7 @@ public class SentinelConnectionManager extends MasterSlaveConnectionManager {
         }
 
         for (Future<RedisPubSubConnection> future : connectionFutures) {
-            future.syncUninterruptibly();
+            future.awaitUninterruptibly();
         }
     }
 
