11eaf5d62e7996de8a26d1326c2fbb166d75c19f
Nikita <abracham.mitchell@gmail.com>
2016-04-12 14:12:13 +0300

    License header fixed

--- src/main/java/org/redisson/client/handler/CommandDecoder.java
+++ src/main/java/org/redisson/client/handler/CommandDecoder.java
@@ -1,4 +1,5 @@
 /**
+ * Copyright 2014 Nikita Koksharov, Nickolay Borbit
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
