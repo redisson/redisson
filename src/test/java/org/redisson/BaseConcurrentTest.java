a5ea392880551303a1defa584130a6e89314c794
jackygurui <jackygurui@gmail.com>
2016-04-21 23:33:18 +0100

    Enhancement to RedissonRuntimeEnvironment
    When running in travis, output uptime and memory usage every 10 seconds
    to make travis’ watch dog happy.

--- src/test/java/org/redisson/BaseConcurrentTest.java
+++ src/test/java/org/redisson/BaseConcurrentTest.java
@@ -1,12 +1,9 @@
 package org.redisson;
 
-import java.lang.management.ManagementFactory;
-import java.lang.management.RuntimeMXBean;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.IntStream;
 
 import org.junit.Assert;
@@ -15,7 +12,7 @@ import org.redisson.client.RedisClient;
 public abstract class BaseConcurrentTest extends BaseTest {
 
     protected void testMultiInstanceConcurrency(int iterations, final RedissonRunnable runnable) throws InterruptedException {
-        System.out.println("Multi Instance Concurrent Job Interation:" + iterations);
+        System.out.println("Multi Instance Concurrent Job Interation: " + iterations);
         ForkJoinPool pool = new ForkJoinPool(Runtime.getRuntime().availableProcessors() * 2);
         final Map<Integer, RedissonClient> instances = new HashMap<>();
 
@@ -29,24 +26,11 @@ public abstract class BaseConcurrentTest extends BaseTest {
         pool.awaitQuiescence(5, TimeUnit.MINUTES);
 
         pool.submit(() -> {
-            final RuntimeMXBean runtimeBean = ManagementFactory.getRuntimeMXBean();
-            final AtomicLong u = new AtomicLong(runtimeBean.getUptime());
             IntStream.range(0, iterations)
                     .parallel()
                     .forEach((i) -> {
-                        if (RedissonRuntimeEnvironment.isTravis) {
-                            long upTime = runtimeBean.getUptime();
-                            if (upTime >= u.get() + 10000) {
-                                u.set(upTime);
-                                System.out.printf("Test Up Time    = %.3f (s)%n", upTime / 1000d);
-                                System.out.printf("Heap Usage      = %.3f (MB)%n", ManagementFactory.getMemoryMXBean().getHeapMemoryUsage().getUsed() / 1024d / 1024d);
-                                System.out.printf("None Heap Usage = %.3f (MB)%n", ManagementFactory.getMemoryMXBean().getNonHeapMemoryUsage().getUsed() / 1024d / 1024d);
-                                System.out.println("=============================");
-                            }
-                        }
                         runnable.run(instances.get(i));
                     });
-            System.out.printf("Test Up Time    = %.3f (s)%n", runtimeBean.getUptime() /1000d);
         });
 
         pool.shutdown();
@@ -67,29 +51,18 @@ public abstract class BaseConcurrentTest extends BaseTest {
     }
 
     protected void testSingleInstanceConcurrency(int iterations, final RedissonRunnable runnable) throws InterruptedException {
-        System.out.println("Single Instance Concurrent Job Interation:" + iterations);
+        System.out.println("Single Instance Concurrent Job Interation: " + iterations);
         final RedissonClient r = BaseTest.createInstance();
         long watch = System.currentTimeMillis();
 
         ForkJoinPool pool = new ForkJoinPool(Runtime.getRuntime().availableProcessors() * 2);
 
         pool.submit(() -> {
-            final RuntimeMXBean runtimeBean = ManagementFactory.getRuntimeMXBean();
-            final AtomicLong u = new AtomicLong(runtimeBean.getUptime());
             IntStream.range(0, iterations)
                     .parallel()
                     .forEach((i) -> {
-                        long upTime = runtimeBean.getUptime();
-                        if (upTime >= u.get() + 10000) {
-                            u.set(upTime);
-                            System.out.printf("Test Up Time    = %.3f (s)%n", upTime / 1000d);
-                            System.out.printf("Heap Usage      = %.3f (MB)%n", ManagementFactory.getMemoryMXBean().getHeapMemoryUsage().getUsed() / 1024d / 1024d);
-                            System.out.printf("None Heap Usage = %.3f (MB)%n", ManagementFactory.getMemoryMXBean().getNonHeapMemoryUsage().getUsed() / 1024d / 1024d);
-                            System.out.println("=============================");
-                        }
                         runnable.run(r);
                     });
-            System.out.printf("Test Up Time    = %.3f (s)%n", runtimeBean.getUptime() / 1000d);
         });
 
         pool.shutdown();
--- src/test/java/org/redisson/RedissonRuntimeEnvironment.java
+++ src/test/java/org/redisson/RedissonRuntimeEnvironment.java
@@ -1,6 +1,12 @@
 package org.redisson;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
 import java.util.Locale;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class RedissonRuntimeEnvironment {
 
@@ -13,5 +19,30 @@ public class RedissonRuntimeEnvironment {
     static {
         OS = System.getProperty("os.name", "generic");
         isWindows = OS.toLowerCase(Locale.ENGLISH).contains("win");
+        if (RedissonRuntimeEnvironment.isTravis) {
+            final AtomicBoolean running = new AtomicBoolean(Boolean.TRUE);
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                running.set(Boolean.FALSE);
+            }));
+            new Thread(() -> {
+                final RuntimeMXBean runtimeBean = ManagementFactory.getRuntimeMXBean();
+                final AtomicLong u = new AtomicLong(runtimeBean.getUptime());
+                while (running.get()) {
+                    try {
+                        long upTime = runtimeBean.getUptime();
+                        if (upTime >= u.get() + 10000) {
+                            u.set(upTime);
+                            System.out.printf("Test Up Time    = %.3f (s)%n", upTime / 1000d);
+                            System.out.printf("Heap Usage      = %.3f (MB)%n", ManagementFactory.getMemoryMXBean().getHeapMemoryUsage().getUsed() / 1024d / 1024d);
+                            System.out.printf("None Heap Usage = %.3f (MB)%n", ManagementFactory.getMemoryMXBean().getNonHeapMemoryUsage().getUsed() / 1024d / 1024d);
+                            System.out.println("=============================");
+                        }
+                        Thread.currentThread().sleep(10000l);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(RedissonRuntimeEnvironment.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }).start();
+        }
     }
 }
