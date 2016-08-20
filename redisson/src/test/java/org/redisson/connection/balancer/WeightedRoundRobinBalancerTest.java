12d72f712435feb86da02f15af95df22732cf3a3
Shailender R. Bathula <shailender.bathula@gmail.com>
2016-08-21 09:42:31 +1200

    #582: Fix up the indentation

--- redisson/src/test/java/org/redisson/connection/balancer/WeightedRoundRobinBalancerTest.java
+++ redisson/src/test/java/org/redisson/connection/balancer/WeightedRoundRobinBalancerTest.java
@@ -17,54 +17,53 @@ import org.redisson.config.Config;
 import org.redisson.config.ReadMode;
 
 public class WeightedRoundRobinBalancerTest {
-	
+
     @Test
-	public void testUseMasterForReadsIfNoConnectionsToSlaves() throws IOException, InterruptedException {
+    public void testUseMasterForReadsIfNoConnectionsToSlaves() throws IOException, InterruptedException {
         RedisProcess master = null;
         RedisProcess slave = null;
         RedissonClient client = null;
         try {
-        	master = redisTestInstance(6379);
-        	slave = redisTestInstance(6380);
-        	
-        	Map<String, Integer> weights = new HashMap<>();
-        	weights.put("127.0.0.1:6379", 1);
-        	weights.put("127.0.0.1:6380", 2);
-        	
-        	Config config = new Config();
-        	config.useMasterSlaveServers()
-	        	.setReadMode(ReadMode.SLAVE)
-	        	.setMasterAddress("127.0.0.1:6379")
-	        	.addSlaveAddress("127.0.0.1:6380")
-	        	.setLoadBalancer(new WeightedRoundRobinBalancer(weights, 1));
-        	
-			client = Redisson.create(config);
-        	
-			// To simulate network connection issues to slave, stop the slave
-			// after creating the client. Cannot create the client without the
-			// slave running. See https://github.com/mrniko/redisson/issues/580
-        	slave.stop();
-        	
-        	RedissonClient clientCopy = client;
-        	await().atMost(5, TimeUnit.SECONDS).until(() -> assertThat(clientCopy.getBucket("key").get()).isNull());
-		} finally {
-			if (master != null) {
-				master.stop();
-			}
-			if (slave != null) {
-				slave.stop();
-			}
-			if (client != null) {
-				client.shutdown();
-			}
-		}
+            master = redisTestInstance(6379);
+            slave = redisTestInstance(6380);
+
+            Map<String, Integer> weights = new HashMap<>();
+            weights.put("127.0.0.1:6379", 1);
+            weights.put("127.0.0.1:6380", 2);
+
+            Config config = new Config();
+            config.useMasterSlaveServers()
+                .setReadMode(ReadMode.SLAVE)
+                .setMasterAddress("127.0.0.1:6379")
+                .addSlaveAddress("127.0.0.1:6380")
+                .setLoadBalancer(new WeightedRoundRobinBalancer(weights, 1));
+
+            client = Redisson.create(config);
+
+            // To simulate network connection issues to slave, stop the slave
+            // after creating the client. Cannot create the client without the
+            // slave running. See https://github.com/mrniko/redisson/issues/580
+            slave.stop();
+
+            RedissonClient clientCopy = client;
+            await().atMost(5, TimeUnit.SECONDS).until(() -> assertThat(clientCopy.getBucket("key").get()).isNull());
+        } finally {
+            if (master != null) {
+                master.stop();
+            }
+            if (slave != null) {
+                slave.stop();
+            }
+            if (client != null) {
+                client.shutdown();
+            }
+        }
     }
-    
-	private RedisProcess redisTestInstance(int port) throws IOException, InterruptedException {
+
+    private RedisProcess redisTestInstance(int port) throws IOException, InterruptedException {
         return new RedisRunner()
                 .nosave()
                 .randomDir()
-                .port(port)
-                .run();
+                .port(port).run();
     }
 }
