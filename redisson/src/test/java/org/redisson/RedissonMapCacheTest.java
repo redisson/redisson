c896e049473f8b1c43ccc5abe19befab3bec5fcb
Rui Gu <jackygurui@gmail.com>
2017-04-04 23:53:12 +0100

    should use double type for test

--- redisson/src/test/java/org/redisson/RedissonMapCacheTest.java
+++ redisson/src/test/java/org/redisson/RedissonMapCacheTest.java
@@ -860,7 +860,7 @@ public class RedissonMapCacheTest extends BaseTest {
         Assert.assertEquals("1", map.get("4"));
         RMapCache<String, Object> mapCache1 = redisson.getMapCache("test_put_if_absent_3");
         Object currValue = mapCache1.putIfAbsent("4", 1.23, 10000L, TimeUnit.SECONDS);
-        Object updatedValue = mapCache1.addAndGet("4", 1L);
+        Object updatedValue = mapCache1.addAndGet("4", 1D);
         System.out.println("updatedValue: " + updatedValue);
     }
 
