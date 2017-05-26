1c7b2a251c3e850e21ff23e0b388cf3f301dd187
Rui Gu <jackygurui@gmail.com>
2017-05-27 00:47:06 +0100

    added redisson-1.1.xsd to support SSL related changes
    changed the creation of RedisClient to use RedisClientConfig
    updated test xml to use 1.1 xsd
    updated wiki test xml to include SSL related configs in reflection to
    changes made in wiki

--- redisson/src/main/java/org/redisson/spring/support/RedisDefinitionParser.java
+++ redisson/src/main/java/org/redisson/spring/support/RedisDefinitionParser.java
@@ -16,6 +16,8 @@
 package org.redisson.spring.support;
 
 import org.redisson.client.RedisClient;
+import org.redisson.client.RedisClientConfig;
+import org.springframework.beans.factory.parsing.BeanComponentDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.xml.AbstractSimpleBeanDefinitionParser;
 import org.springframework.beans.factory.xml.ParserContext;
@@ -26,20 +28,21 @@ import org.w3c.dom.Element;
  *
  * @author Rui Gu (https://github.com/jackygurui)
  */
-public final class RedisDefinitionParser 
+public final class RedisDefinitionParser
         extends AbstractSimpleBeanDefinitionParser {
-    
+
+    private static final String ADDRESS_ATTRIBUTE = "address";
     private static final String HOST_ATTRIBUTE = "host";
     private static final String PORT_ATTRIBUTE = "port";
     private static final String CONNECTION_TIMEOUT_ATTRIBUTE = "connectionTimeout";
     private static final String COMMAND_TIMEOUT_ATTRIBUTE = "commandTimeout";
-    
+
     private final RedissonNamespaceParserSupport helper;
-    
+
     public RedisDefinitionParser(RedissonNamespaceParserSupport helper) {
         this.helper = helper;
     }
-    
+
     @Override
     protected Class<RedisClient> getBeanClass(Element element) {
         return RedisClient.class;
@@ -48,14 +51,27 @@ public final class RedisDefinitionParser
     @Override
     protected void doParse(Element element, ParserContext parserContext, BeanDefinitionBuilder builder) {
         builder.getRawBeanDefinition().setBeanClass(RedisClient.class);
-        helper.addConstructorArgs(element,
-                HOST_ATTRIBUTE, String.class, builder);
-        helper.addConstructorArgs(element,
-                PORT_ATTRIBUTE, int.class, builder);
-        helper.addConstructorArgs(element,
-                CONNECTION_TIMEOUT_ATTRIBUTE, int.class, builder);
-        helper.addConstructorArgs(element,
-                COMMAND_TIMEOUT_ATTRIBUTE, int.class, builder);
+        if (helper.hasAttribute(element, HOST_ATTRIBUTE)) {
+            helper.addConstructorArgs(element,
+                    HOST_ATTRIBUTE, String.class, builder);
+            helper.addConstructorArgs(element,
+                    PORT_ATTRIBUTE, int.class, builder);
+            helper.addConstructorArgs(element,
+                    CONNECTION_TIMEOUT_ATTRIBUTE, int.class, builder);
+            helper.addConstructorArgs(element,
+                    COMMAND_TIMEOUT_ATTRIBUTE, int.class, builder);
+        } else {
+            BeanDefinitionBuilder b
+                    = helper.createBeanDefinitionBuilder(element,
+                            parserContext,
+                            RedisClientConfig.class);
+            String configId = helper.getId(null, b, parserContext);
+            helper.parseAttributes(element, parserContext, b);
+            BeanComponentDefinition def
+                    = helper.registerBeanDefinition(b, configId,
+                            null, parserContext);
+            helper.addConstructorArgs(def, RedisClientConfig.class, builder);
+        }
         builder.setDestroyMethodName("shutdown");
         parserContext.getDelegate().parseQualifierElements(element,
                 builder.getRawBeanDefinition());
@@ -65,7 +81,7 @@ public final class RedisDefinitionParser
     protected boolean shouldGenerateIdAsFallback() {
         return true;
     }
-    
+
     @Override
     protected boolean isEligibleAttribute(String attributeName) {
         return helper.isEligibleAttribute(attributeName);
