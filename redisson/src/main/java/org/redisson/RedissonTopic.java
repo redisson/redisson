820579b54164a93841310b09c251ed7a55f15f23
Nikita <abracham.mitchell@gmail.com>
2017-09-01 14:18:22 +0300

    compilation fixed

--- redisson/src/main/java/org/redisson/RedissonTopic.java
+++ redisson/src/main/java/org/redisson/RedissonTopic.java
@@ -108,7 +108,7 @@ public class RedissonTopic<M> implements RTopic<M> {
     
     public RFuture<Integer> addListenerAsync(final RedisPubSubListener<?> pubSubListener) {
         RFuture<PubSubConnectionEntry> future = commandExecutor.getConnectionManager().subscribe(codec, name, pubSubListener);
-        RPromise<Integer> result = new RedissonPromise<Integer>();
+        final RPromise<Integer> result = new RedissonPromise<Integer>();
         future.addListener(new FutureListener<PubSubConnectionEntry>() {
             @Override
             public void operationComplete(Future<PubSubConnectionEntry> future) throws Exception {
