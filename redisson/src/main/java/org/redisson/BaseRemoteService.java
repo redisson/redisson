d3b298940218d2d49b0dbf12cbc0fc6a3d0fbdb1
Nikita <abracham.mitchell@gmail.com>
2018-01-04 18:11:07 +0300

    JDK 1.6+ compatibility fixed. #1209

--- redisson/src/main/java/org/redisson/BaseRemoteService.java
+++ redisson/src/main/java/org/redisson/BaseRemoteService.java
@@ -77,7 +77,7 @@ public abstract class BaseRemoteService {
     private static final Logger log = LoggerFactory.getLogger(BaseRemoteService.class);
 
     private final Map<Class<?>, String> requestQueueNameCache = PlatformDependent.newConcurrentHashMap();
-    private final Map<Method, List<String>> methodSignaturesCache = PlatformDependent.newConcurrentHashMap();
+    private final ConcurrentMap<Method, List<String>> methodSignaturesCache = PlatformDependent.newConcurrentHashMap();
 
     protected final Codec codec;
     protected final RedissonClient redisson;
