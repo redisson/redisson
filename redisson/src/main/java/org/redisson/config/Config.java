f0b181d35d0ae93ac60ae6f1be90ca3cf43530f2
Hai Saadon <hai.saadon@microsoft.com>
2018-02-15 16:54:18 +0200

    add ability to use RoundRobinDnsAddressResolverGroup in Connection manager.

--- redisson/src/main/java/org/redisson/config/Config.java
+++ redisson/src/main/java/org/redisson/config/Config.java
@@ -27,6 +27,7 @@ import org.redisson.codec.DefaultReferenceCodecProvider;
 import org.redisson.codec.JsonJacksonCodec;
 import org.redisson.codec.ReferenceCodecProvider;
 import org.redisson.connection.ConnectionManager;
+import org.redisson.connection.AddressResolverGroupFactory;
 import org.redisson.connection.ReplicatedConnectionManager;
 
 import io.netty.channel.EventLoopGroup;
@@ -86,6 +87,11 @@ public class Config {
     
     private boolean keepPubSubOrder = true;
     
+    /**
+     * AddressResolverGroupFactory switch between default and round robin
+     */
+    private AddressResolverGroupFactory addressResolverGroupFactory = AddressResolverGroupFactory.DNS_ADDRESS_RESOLVER_GROUP;
+
     public Config() {
     }
 
@@ -107,7 +113,8 @@ public class Config {
         setReferenceEnabled(oldConf.isReferenceEnabled());
         setEventLoopGroup(oldConf.getEventLoopGroup());
         setTransportMode(oldConf.getTransportMode());
-        
+        setAddressResolverGroupFactory(oldConf.getAddressResolverGroupFactory());
+
         if (oldConf.getSingleServerConfig() != null) {
             setSingleServerConfig(new SingleServerConfig(oldConf.getSingleServerConfig()));
         }
@@ -591,6 +598,13 @@ public class Config {
         return keepPubSubOrder;
     }
 
+    public void setAddressResolverGroupFactory(AddressResolverGroupFactory addressResolverGroupFactory) {
+        this.addressResolverGroupFactory = addressResolverGroupFactory;
+    }
+
+    public AddressResolverGroupFactory getAddressResolverGroupFactory() {
+        return addressResolverGroupFactory;
+    }
 
     /**
      * Read config object stored in JSON format from <code>String</code>
