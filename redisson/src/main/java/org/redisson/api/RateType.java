733f64d5afe8abf99f46242cc0fc45c17c89dd3b
Nikita <abracham.mitchell@gmail.com>
2018-05-23 09:47:05 +0300

    RRateLimiter added. #75

---
+++ redisson/src/main/java/org/redisson/api/RateType.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright 2018 Nikita Koksharov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.redisson.api;
+
+/**
+ * 
+ * @author Nikita Koksharov
+ *
+ */
+public enum RateType {
+
+    /**
+     * Total rate for all RateLimiter instances
+     */
+    OVERALL,
+    
+    /**
+     * Total rate for all RateLimiter instances working with the same Redisson instance
+     */
+    PER_CLIENT
+    
+}
