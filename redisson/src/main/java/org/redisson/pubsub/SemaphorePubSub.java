38d279f5d2605af2e0a581d5c3ec37c456c2ca22
Nikita <abracham.mitchell@gmail.com>
2018-06-01 10:38:43 +0300

    Fixed - methods belongs to transactional objects get blocked at high concurrency. #1459

--- redisson/src/main/java/org/redisson/pubsub/SemaphorePubSub.java
+++ redisson/src/main/java/org/redisson/pubsub/SemaphorePubSub.java
@@ -32,27 +32,16 @@ public class SemaphorePubSub extends PublishSubscribe<RedissonLockEntry> {
 
     @Override
     protected void onMessage(RedissonLockEntry value, Long message) {
-        value.getLatch().release(message.intValue());
-        
         while (true) {
-            Runnable runnableToExecute = null;
-            synchronized (value) {
-                Runnable runnable = value.getListeners().poll();
-                if (runnable != null) {
-                    if (value.getLatch().tryAcquire()) {
-                        runnableToExecute = runnable;
-                    } else {
-                        value.addListener(runnable);
-                    }
-                }
+            Runnable runnableToExecute = value.getListeners().poll();
+            if (runnableToExecute == null) {
+                break;
             }
             
-            if (runnableToExecute != null) {
-                runnableToExecute.run();
-            } else {
-                return;
-            }
+            runnableToExecute.run();
         }
+        
+        value.getLatch().release(message.intValue());
     }
 
 }
