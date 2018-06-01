c9b5913018f5587c29da61e70245b690a5b46601
Nikita <abracham.mitchell@gmail.com>
2018-06-01 11:32:02 +0300

    Fixed - Redis nodes with noaddr flag should be parsed correctly.

--- redisson/src/main/java/org/redisson/cluster/ClusterNodeInfo.java
+++ redisson/src/main/java/org/redisson/cluster/ClusterNodeInfo.java
@@ -16,6 +16,7 @@
 package org.redisson.cluster;
 
 import java.net.URI;
+import java.util.EnumSet;
 import java.util.HashSet;
 import java.util.Set;
 import org.redisson.misc.URIBuilder;
@@ -33,7 +34,7 @@ public class ClusterNodeInfo {
     
     private String nodeId;
     private URI address;
-    private final Set<Flag> flags = new HashSet<Flag>();
+    private final Set<Flag> flags = EnumSet.noneOf(Flag.class);
     private String slaveOf;
 
     private final Set<ClusterSlotRange> slotRanges = new HashSet<ClusterSlotRange>();
