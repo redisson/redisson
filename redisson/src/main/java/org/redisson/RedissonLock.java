08e130d8978110caf0344b33e2df6fb0e1309d06
Kevin Martin <kvn.p.mrtn@gmail.com>
2018-09-04 11:45:53 -0700

    Small Optimization

--- redisson/src/main/java/org/redisson/RedissonLock.java
+++ redisson/src/main/java/org/redisson/RedissonLock.java
@@ -249,16 +249,8 @@ public class RedissonLock extends RedissonExpirable implements RLock {
         internalLockLeaseTime = unit.toMillis(leaseTime);
 
         return commandExecutor.evalWriteAsync(getName(), LongCodec.INSTANCE, command,
-                  "if (redis.call('exists', KEYS[1]) == 0) then " +
-                      "redis.call('hset', KEYS[1], ARGV[2], 1); " +
-                      "redis.call('pexpire', KEYS[1], ARGV[1]); " +
-                      "return nil; " +
-                  "end; " +
-                  "if (redis.call('hexists', KEYS[1], ARGV[2]) == 1) then " +
-                      "redis.call('hincrby', KEYS[1], ARGV[2], 1); " +
-                      "redis.call('pexpire', KEYS[1], ARGV[1]); " +
-                      "return nil; " +
-                  "end; " +
+                  "redis.call('hincrby', KEYS[1], ARGV[2], 1); " +
+                  "redis.call('pexpire', KEYS[1], ARGV[1]); " +
                   "return redis.call('pttl', KEYS[1]);",
                     Collections.<Object>singletonList(getName()), internalLockLeaseTime, getLockName(threadId));
     }
