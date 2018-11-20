1ba74786794e412a7abaa2a7688ba5e6df8d7f6b
Nikita Koksharov <nkoksharov@redisson.pro>
2018-11-20 17:29:05 +0300

    javadoc fixed

--- redisson/src/main/java/org/redisson/api/RTopicRx.java
+++ redisson/src/main/java/org/redisson/api/RTopicRx.java
@@ -77,6 +77,7 @@ public interface RTopicRx {
     /**
      * Returns stream of messages.
      * 
+     * @param <M> - type of message
      * @param type - type of message to listen
      * @return stream of messages
      */
