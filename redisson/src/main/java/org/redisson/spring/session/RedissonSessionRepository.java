de1f34498a1d8e45ae128163c56f8de62079db01
Nikita Koksharov <nkoksharov@redisson.pro>
2018-11-26 17:52:38 +0300

    Fixed - Spring Session keyPrefix setting isn't used in session name. #1755

--- redisson/src/main/java/org/redisson/spring/session/RedissonSessionRepository.java
+++ redisson/src/main/java/org/redisson/spring/session/RedissonSessionRepository.java
@@ -57,9 +57,9 @@ public class RedissonSessionRepository implements FindByIndexNameSessionReposito
         private final MapSession delegate;
         private RMap<String, Object> map;
 
-        public RedissonSession() {
+        public RedissonSession(String keyPrefix) {
             this.delegate = new MapSession();
-            map = redisson.getMap("redisson_spring_session:" + delegate.getId());
+            map = redisson.getMap(keyPrefix + delegate.getId());
             principalName = resolvePrincipal(delegate);
 
             Map<String, Object> newMap = new HashMap<String, Object>(3);
@@ -81,9 +81,9 @@ public class RedissonSessionRepository implements FindByIndexNameSessionReposito
             }
         }
         
-        public RedissonSession(String sessionId) {
+        public RedissonSession(String keyPrefix, String sessionId) {
             this.delegate = new MapSession(sessionId);
-            map = redisson.getMap("redisson_spring_session:" + sessionId);
+            map = redisson.getMap(keyPrefix + sessionId);
             principalName = resolvePrincipal(delegate);
         }
         
@@ -290,7 +290,7 @@ public class RedissonSessionRepository implements FindByIndexNameSessionReposito
 
     @Override
     public RedissonSession createSession() {
-        RedissonSession session = new RedissonSession();
+        RedissonSession session = new RedissonSession(keyPrefix);
         if (defaultMaxInactiveInterval != null) {
             session.setMaxInactiveIntervalInSeconds(defaultMaxInactiveInterval);
         }
