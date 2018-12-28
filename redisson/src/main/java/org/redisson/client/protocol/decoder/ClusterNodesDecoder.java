2b25f3a4e5758590d40ecc82cca1db7bc39864a7
John Spaetzel <john.spaetzel@hellosign.com>
2018-12-27 17:07:06 -0800

    Use implicit localhost when not provided by cluster nodes

--- redisson/src/main/java/org/redisson/client/protocol/decoder/ClusterNodesDecoder.java
+++ redisson/src/main/java/org/redisson/client/protocol/decoder/ClusterNodesDecoder.java
@@ -65,8 +65,11 @@ public class ClusterNodesDecoder implements Decoder<List<ClusterNodeInfo>> {
                 if (ssl) {
                     protocol = "rediss://";
                 }
-                String addr = protocol + params[1].split("@")[0];
-                node.setAddress(addr);
+                String addr =  params[1].split("@")[0];
+                if (params[1].charAt(0) == ':') {
+                    addr = "127.0.0.1" + addr;
+                }
+                node.setAddress(protocol + addr);
             }
 
             String slaveOf = params[3];
