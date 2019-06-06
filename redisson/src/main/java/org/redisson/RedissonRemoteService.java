405ca0a99289bfba3f3638299d86536fafa246f7
slyao <slyao@tencent.com>
2019-06-06 21:51:07 +0800

    Fixed - RRemoteService.getFreeWorkers api will remove the entry from remoteMap, then only get zero. #2151

--- redisson/src/main/java/org/redisson/RedissonRemoteService.java
+++ redisson/src/main/java/org/redisson/RedissonRemoteService.java
@@ -136,7 +136,7 @@ public class RedissonRemoteService extends BaseRemoteService implements RRemoteS
     
     @Override
     public int getFreeWorkers(Class<?> remoteInterface) {
-        Entry entry = remoteMap.remove(remoteInterface);
+        Entry entry = remoteMap.get(remoteInterface);
         if (entry == null) {
             return 0;
         }
