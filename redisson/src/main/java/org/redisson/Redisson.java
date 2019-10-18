43eb732dbbbcdab032af124404cab361c1c9a588
shaileshmishra008 <shaileshmishra_008@yahoo.com>
2019-10-18 13:05:30 +0530

    Update Redisson.java

--- redisson/src/main/java/org/redisson/Redisson.java
+++ redisson/src/main/java/org/redisson/Redisson.java
@@ -306,6 +306,16 @@ public class Redisson implements RedissonClient {
         return new RedissonLocalCachedMap<K, V>(codec, connectionManager.getCommandExecutor(), name, 
                 options, evictionScheduler, this, writeBehindService);
     }
+    
+    @Override
+    public <K, V> RLocalCachedMap<K, V> getLocalCachedMap(String name) {
+        return getLocalCachedMap(name, LocalCachedMapOptions.defaults());
+    }
+
+    @Override
+    public <K, V> RLocalCachedMap<K, V> getLocalCachedMap(String name, Codec codec) {
+        return getLocalCachedMap(name, codec, LocalCachedMapOptions.defaults());
+    }
 
     @Override
     public <K, V> RMap<K, V> getMap(String name) {
