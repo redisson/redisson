b919599b291c4e6806cb5842dcb4772cc454f01a
Nikita Koksharov <nkoksharov@redisson.pro>
2019-12-10 15:42:40 +0300

    refactoring

--- redisson/src/main/java/org/redisson/misc/ReclosableLatch.java
+++ redisson/src/main/java/org/redisson/misc/ReclosableLatch.java
@@ -13,6 +13,24 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/**
+ *    Copyright 2009 - 2017, Red Hat Inc. and/or its affiliates.
+ *
+ *    All files in this repository or distribution are licensed under the
+ *    Apache License, Version 2.0 (the "License");
+ *    you may not use any files in this repository or distribution except
+ *    in compliance with the License.
+ *
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package org.redisson.misc;
 
 import java.util.concurrent.TimeUnit;
