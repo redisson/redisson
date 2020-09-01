843be7d6ccacd3d517ca553e3721d972fa5e2714
Nikita Koksharov <nkoksharov@redisson.pro>
2020-09-01 15:30:13 +0300

    Fixed - compatibility with Spring Data Redis 2.3.3

--- redisson-spring-data/redisson-spring-data-23/src/main/java/org/redisson/spring/data/connection/RedissonReactiveRedisClusterConnection.java
+++ redisson-spring-data/redisson-spring-data-23/src/main/java/org/redisson/spring/data/connection/RedissonReactiveRedisClusterConnection.java
@@ -15,11 +15,20 @@
  */
 package org.redisson.spring.data.connection;
 
+import org.redisson.client.codec.ByteArrayCodec;
+import org.redisson.client.codec.StringCodec;
 import org.redisson.client.protocol.RedisCommands;
+import org.redisson.client.protocol.RedisStrictCommand;
+import org.redisson.client.protocol.decoder.ObjectListReplayDecoder;
 import org.redisson.reactive.CommandReactiveExecutor;
 import org.springframework.data.redis.connection.*;
-
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+
+import java.nio.ByteBuffer;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * 
@@ -92,4 +101,125 @@ public class RedissonReactiveRedisClusterConnection extends RedissonReactiveRedi
         return execute(node, RedisCommands.PING);
     }
 
+    private static final RedisStrictCommand<List<RedisClusterNode>> CLUSTER_NODES =
+                            new RedisStrictCommand<>("CLUSTER", "NODES", new RedisClusterNodeDecoder());
+
+    @Override
+    public Flux<RedisClusterNode> clusterGetNodes() {
+        Mono<List<RedisClusterNode>> result = read(null, StringCodec.INSTANCE, CLUSTER_NODES);
+        return result.flatMapMany(e -> Flux.fromIterable(e));
+    }
+
+    @Override
+    public Flux<RedisClusterNode> clusterGetSlaves(RedisClusterNode redisClusterNode) {
+        Flux<RedisClusterNode> nodes = clusterGetNodes();
+        Flux<RedisClusterNode> master = nodes.filter(e -> e.getHost().equals(redisClusterNode.getHost()) && e.getPort().equals(redisClusterNode.getPort()));
+        return master.flatMap(node -> clusterGetNodes().filter(e -> Objects.equals(e.getMasterId(), node.getMasterId())));
+    }
+
+    @Override
+    public Mono<Map<RedisClusterNode, Collection<RedisClusterNode>>> clusterGetMasterSlaveMap() {
+        Flux<RedisClusterNode> nodes = clusterGetNodes();
+        Flux<RedisClusterNode> masters = nodes.filter(e -> e.isMaster());
+        return masters.flatMap(master -> Mono.just(master).zipWith(clusterGetNodes()
+                                        .filter(e -> Objects.equals(e.getMasterId(), master.getMasterId()))
+                                        .collect(Collectors.toSet())))
+                      .collect(Collectors.toMap(Tuple2::getT1, Tuple2::getT2));
+    }
+
+    @Override
+    public Mono<Integer> clusterGetSlotForKey(ByteBuffer byteBuffer) {
+        return read(null, StringCodec.INSTANCE, RedisCommands.KEYSLOT, toByteArray(byteBuffer));
+    }
+
+    @Override
+    public Mono<RedisClusterNode> clusterGetNodeForSlot(int slot) {
+        return clusterGetNodes().filter(n -> n.isMaster() && n.getSlotRange().contains(slot)).next();
+    }
+
+    @Override
+    public Mono<RedisClusterNode> clusterGetNodeForKey(ByteBuffer byteBuffer) {
+        int slot = executorService.getConnectionManager().calcSlot(toByteArray(byteBuffer));
+        return clusterGetNodeForSlot(slot);
+    }
+
+    @Override
+    public Mono<ClusterInfo> clusterGetClusterInfo() {
+        Mono<Map<String, String>> mono = read(null, StringCodec.INSTANCE, RedisCommands.CLUSTER_INFO);
+        return mono.map(e -> {
+            Properties props = new Properties();
+            for (Map.Entry<String, String> entry : e.entrySet()) {
+                props.setProperty(entry.getKey(), entry.getValue());
+            }
+            return new ClusterInfo(props);
+        });
+    }
+
+    @Override
+    public Mono<Void> clusterAddSlots(RedisClusterNode redisClusterNode, int... ints) {
+        List<Integer> params = convert(ints);
+        return execute(redisClusterNode, RedisCommands.CLUSTER_ADDSLOTS, params.toArray());
+    }
+
+    private List<Integer> convert(int... slots) {
+        List<Integer> params = new ArrayList<Integer>();
+        for (int slot : slots) {
+            params.add(slot);
+        }
+        return params;
+    }
+
+    @Override
+    public Mono<Void> clusterAddSlots(RedisClusterNode redisClusterNode, RedisClusterNode.SlotRange slotRange) {
+        return clusterAddSlots(redisClusterNode, slotRange.getSlotsArray());
+    }
+
+    @Override
+    public Mono<Long> clusterCountKeysInSlot(int slot) {
+        Mono<RedisClusterNode> node = clusterGetNodeForSlot(slot);
+        return node.flatMap(e -> {
+            return execute(e, RedisCommands.CLUSTER_COUNTKEYSINSLOT, slot);
+        });
+    }
+
+    @Override
+    public Mono<Void> clusterDeleteSlots(RedisClusterNode redisClusterNode, int... ints) {
+        List<Integer> params = convert(ints);
+        return execute(redisClusterNode, RedisCommands.CLUSTER_DELSLOTS, params.toArray());
+    }
+
+    @Override
+    public Mono<Void> clusterDeleteSlotsInRange(RedisClusterNode redisClusterNode, RedisClusterNode.SlotRange slotRange) {
+        return clusterDeleteSlots(redisClusterNode, slotRange.getSlotsArray());
+    }
+
+    @Override
+    public Mono<Void> clusterForget(RedisClusterNode redisClusterNode) {
+        return execute(redisClusterNode, RedisCommands.CLUSTER_FORGET, redisClusterNode.getId());
+    }
+
+    @Override
+    public Mono<Void> clusterMeet(RedisClusterNode redisClusterNode) {
+        return execute(redisClusterNode, RedisCommands.CLUSTER_MEET, redisClusterNode.getHost(), redisClusterNode.getPort());
+    }
+
+    @Override
+    public Mono<Void> clusterSetSlot(RedisClusterNode redisClusterNode, int slot, AddSlots addSlots) {
+        return execute(redisClusterNode, RedisCommands.CLUSTER_SETSLOT, slot, addSlots);
+    }
+
+    private static final RedisStrictCommand<List<String>> CLUSTER_GETKEYSINSLOT = new RedisStrictCommand<List<String>>("CLUSTER", "GETKEYSINSLOT", new ObjectListReplayDecoder<String>());
+
+    @Override
+    public Flux<ByteBuffer> clusterGetKeysInSlot(int slot, int count) {
+        Mono<List<byte[]>> f = executorService.reactive(() -> {
+            return executorService.readAsync((String) null, ByteArrayCodec.INSTANCE, CLUSTER_GETKEYSINSLOT, slot, count);
+        });
+        return f.flatMapMany(e -> Flux.fromIterable(e)).map(e -> ByteBuffer.wrap(e));
+    }
+
+    @Override
+    public Mono<Void> clusterReplicate(RedisClusterNode redisClusterNode, RedisClusterNode slave) {
+        return execute(redisClusterNode, RedisCommands.CLUSTER_REPLICATE, slave.getId());
+    }
 }
