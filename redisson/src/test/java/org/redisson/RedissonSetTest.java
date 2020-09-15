5d56315cad471dbe4d1d6bb824367d9575d61594
Nikita Koksharov <nkoksharov@redisson.pro>
2020-09-15 09:43:11 +0300

    test fixed

--- redisson/src/test/java/org/redisson/RedissonSetTest.java
+++ redisson/src/test/java/org/redisson/RedissonSetTest.java
@@ -824,7 +824,7 @@ public class RedissonSetTest extends BaseTest {
 
         Assert.assertTrue(list.removeAll(Arrays.asList(4)));
 
-        assertThat(list).containsExactly(1, 5);
+        assertThat(list).containsExactlyInAnyOrder(1, 5);
 
         Assert.assertTrue(list.removeAll(Arrays.asList(1, 5, 1, 5)));
 
