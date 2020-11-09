f4c582e1efa12bd66d373a12a3bd29fd6c65a31f
王彬 <burgleaf@163.com>
2020-11-09 15:15:22 +0800

    修改redisson断线重连后无法再续期的问题

--- redisson/src/main/java/org/redisson/RedissonLock.java
+++ redisson/src/main/java/org/redisson/RedissonLock.java
@@ -288,6 +288,7 @@ public class RedissonLock extends RedissonExpirable implements RLock {
                 future.onComplete((res, e) -> {
                     if (e != null) {
                         log.error("Can't update lock " + getName() + " expiration", e);
+                        EXPIRATION_RENEWAL_MAP.remove(getEntryName());
                         return;
                     }
                     
