101bf8ba22734da820e5c321ecde8acd1d75d4b8
Nikita Koksharov <nkoksharov@redisson.pro>
2020-12-09 13:30:42 +0300

    refactoring

--- redisson/src/main/java/org/redisson/command/CommandAsyncService.java
+++ redisson/src/main/java/org/redisson/command/CommandAsyncService.java
@@ -759,9 +759,7 @@ public class CommandAsyncService implements CommandAsyncExecutor {
         } else {
             List<Object> params = new ArrayList<Object>(queueNames.length + 1);
             params.add(name);
-            for (Object queueName : queueNames) {
-                params.add(queueName);
-            }
+            params.addAll(Arrays.asList(queueNames));
             params.add(secondsTimeout);
             return writeAsync(name, codec, command, params.toArray());
         }
