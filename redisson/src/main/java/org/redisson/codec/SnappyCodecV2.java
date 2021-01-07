26cc1e58add8a7d41f7e82205f4756577769c6c5
Nikita Koksharov <nkoksharov@redisson.pro>
2021-01-07 08:13:02 +0300

    javadocs fixed

--- redisson/src/main/java/org/redisson/codec/SnappyCodecV2.java
+++ redisson/src/main/java/org/redisson/codec/SnappyCodecV2.java
@@ -29,11 +29,13 @@ import java.io.IOException;
 /**
  * Google's Snappy compression codec.
  * Uses inner <code>Codec</code> to convert object to binary stream.
- * <code>FstCodec</code> used by default.
+ * <code>MarshallingCodec</code> used by default.
  * <p>
  * Based on <a href="https://github.com/xerial/snappy-java">https://github.com/xerial/snappy-java</a>
  *
- * @see org.redisson.codec.FstCodec
+ * Fully thread-safe.
+ *
+ * @see org.redisson.codec.MarshallingCodec
  *
  * @author Nikita Koksharov
  *
