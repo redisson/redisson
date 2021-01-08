85610a0064164cd08b15717226ca1e794c1a32f5
Nikita Koksharov <nkoksharov@redisson.pro>
2021-01-08 14:38:17 +0300

    Fixed - InterruptedExceptions are hidden by RedisException. #3324

--- redisson/src/main/java/org/redisson/command/CommandAsyncService.java
+++ redisson/src/main/java/org/redisson/command/CommandAsyncService.java
@@ -139,6 +139,7 @@ public class CommandAsyncService implements CommandAsyncExecutor {
         try {
             future.await();
         } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
             throw new RedisException(e);
         }
         if (future.isSuccess()) {
