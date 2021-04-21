00ed5ff3dc675af81102dad5dbf0d1a3d09fdeaa
Nikita Koksharov <nkoksharov@redisson.pro>
2021-04-21 10:16:20 +0300

    Fixed - Redisson tries reconnect to Redis nodes which marked as shutdown by topology manager. #3560

--- redisson/src/main/java/org/redisson/client/handler/ConnectionWatchdog.java
+++ redisson/src/main/java/org/redisson/client/handler/ConnectionWatchdog.java
@@ -100,8 +100,10 @@ public class ConnectionWatchdog extends ChannelInboundHandlerAdapter {
         }
     }
 
-    private void tryReconnect(final RedisConnection connection, final int nextAttempt) {
-        if (connection.isClosed() || bootstrap.config().group().isShuttingDown()) {
+    private void tryReconnect(RedisConnection connection, int nextAttempt) {
+        if (connection.getRedisClient().isShutdown()
+                || connection.isClosed()
+                    || bootstrap.config().group().isShuttingDown()) {
             return;
         }
 
@@ -112,7 +114,9 @@ public class ConnectionWatchdog extends ChannelInboundHandlerAdapter {
 
                 @Override
                 public void operationComplete(final ChannelFuture future) throws Exception {
-                    if (connection.isClosed() || bootstrap.config().group().isShuttingDown()) {
+                    if (connection.getRedisClient().isShutdown()
+                            || connection.isClosed()
+                                || bootstrap.config().group().isShuttingDown()) {
                         if (future.isSuccess()) {
                             Channel ch = future.channel();
                             RedisConnection con = RedisConnection.getFrom(ch);
@@ -133,12 +137,14 @@ public class ConnectionWatchdog extends ChannelInboundHandlerAdapter {
                             RedisConnection c = RedisConnection.getFrom(channel);
                             c.getConnectionPromise().onComplete((res, e) -> {
                                 if (e == null) {
-                                    refresh(connection, channel);
-                                    if (!connection.isClosed()) {
-                                        log.debug("{} connected to {}, command: {}", connection, connection.getRedisClient().getAddr(), connection.getCurrentCommand());
-                                    } else {
+                                    if (connection.getRedisClient().isShutdown()
+                                            || connection.isClosed()) {
                                         channel.close();
+                                        return;
+                                    } else {
+                                        log.debug("{} connected to {}, command: {}", connection, connection.getRedisClient().getAddr(), connection.getCurrentCommand());
                                     }
+                                    refresh(connection, channel);
                                 } else {
                                     channel.close();
                                     reconnect(connection, nextAttempt);
