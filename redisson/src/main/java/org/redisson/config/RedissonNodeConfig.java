877127c7fddbb2b9eeb4afaccad7a7e9a4b6a5aa
Nikita Koksharov <nkoksharov@redisson.pro>
2021-05-19 12:30:30 +0300

    Fixed - RedissonNode unable to start due to ClassNotFoundException #3621
    Signed-off-by: kurenchuksergey <kurenchuksergey@gmail.com>

--- redisson/src/main/java/org/redisson/config/RedissonNodeConfig.java
+++ redisson/src/main/java/org/redisson/config/RedissonNodeConfig.java
@@ -15,12 +15,6 @@
  */
 package org.redisson.config;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
-import org.redisson.api.RedissonNodeInitializer;
 import org.springframework.beans.factory.BeanFactory;
 
 /**
@@ -29,13 +23,10 @@ import org.springframework.beans.factory.BeanFactory;
  * @author Nikita Koksharov
  *
  */
-public class RedissonNodeConfig extends Config {
+public class RedissonNodeConfig extends RedissonNodeFileConfig {
     
-    private int mapReduceWorkers = 0;
-    private RedissonNodeInitializer redissonNodeInitializer;
     private BeanFactory beanFactory;
-    private Map<String, Integer> executorServiceWorkers = new HashMap<String, Integer>();
-    
+
     public RedissonNodeConfig() {
         super();
     }
@@ -46,61 +37,9 @@ public class RedissonNodeConfig extends Config {
     
     public RedissonNodeConfig(RedissonNodeConfig oldConf) {
         super(oldConf);
-        this.executorServiceWorkers = new HashMap<String, Integer>(oldConf.executorServiceWorkers);
-        this.redissonNodeInitializer = oldConf.redissonNodeInitializer;
-        this.mapReduceWorkers = oldConf.mapReduceWorkers;
         this.beanFactory = oldConf.beanFactory;
     }
     
-    /**
-     * MapReduce workers amount. 
-     * <p>
-     * <code>0 = current_processors_amount</code>
-     * <p>
-     * <code>-1 = disable MapReduce workers</code>
-     * 
-     * <p>
-     * Default is <code>0</code>
-     * 
-     * @param mapReduceWorkers workers for MapReduce
-     * @return config
-     */
-    public RedissonNodeConfig setMapReduceWorkers(int mapReduceWorkers) {
-        this.mapReduceWorkers = mapReduceWorkers;
-        return this;
-    }
-    public int getMapReduceWorkers() {
-        return mapReduceWorkers;
-    }
-    
-    /**
-     * Executor service workers amount per service name 
-     * 
-     * @param workers mapping
-     * @return config
-     */
-    public RedissonNodeConfig setExecutorServiceWorkers(Map<String, Integer> workers) {
-        this.executorServiceWorkers = workers;
-        return this;
-    }
-    public Map<String, Integer> getExecutorServiceWorkers() {
-        return executorServiceWorkers;
-    }
-    
-    /**
-     * Redisson node initializer
-     * 
-     * @param redissonNodeInitializer object
-     * @return config
-     */
-    public RedissonNodeConfig setRedissonNodeInitializer(RedissonNodeInitializer redissonNodeInitializer) {
-        this.redissonNodeInitializer = redissonNodeInitializer;
-        return this;
-    }
-    public RedissonNodeInitializer getRedissonNodeInitializer() {
-        return redissonNodeInitializer;
-    }
-
     public BeanFactory getBeanFactory() {
         return beanFactory;
     }
@@ -115,28 +54,4 @@ public class RedissonNodeConfig extends Config {
         this.beanFactory = beanFactory;
     }
 
-    /**
-     * Read config object stored in JSON format from <code>File</code>
-     *
-     * @param file object
-     * @return config
-     * @throws IOException error
-     */
-    public static RedissonNodeConfig fromJSON(File file) throws IOException {
-        ConfigSupport support = new ConfigSupport();
-        return support.fromJSON(file, RedissonNodeConfig.class);
-    }
-
-    /**
-     * Read config object stored in YAML format from <code>File</code>
-     *
-     * @param file object
-     * @return config
-     * @throws IOException error
-     */
-    public static RedissonNodeConfig fromYAML(File file) throws IOException {
-        ConfigSupport support = new ConfigSupport();
-        return support.fromYAML(file, RedissonNodeConfig.class);
-    }
-    
 }
