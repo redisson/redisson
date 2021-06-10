e9a7703eb4ce0d80b689801f57e9932672ee079d
Nikita Koksharov <nkoksharov@redisson.pro>
2021-06-10 11:18:16 +0300

    license header check added

--- redisson-quarkus/redisson-quarkus-16/runtime/src/main/java/io/quarkus/redisson/client/runtime/graal/NettySubstitutions.java
+++ redisson-quarkus/redisson-quarkus-16/runtime/src/main/java/io/quarkus/redisson/client/runtime/graal/NettySubstitutions.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2013-2021 Nikita Koksharov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 /*
  * Copyright 2020 The Netty Project
  *
@@ -53,7 +68,6 @@ final class Quarkus1Condition implements BooleanSupplier {
 
     @Override
     public boolean getAsBoolean() {
-        System.out.println("getAsBoolean " + Quarkus.class.getPackage().getImplementationVersion());
         return Quarkus.class.getPackage().getImplementationVersion().startsWith("1.");
     }
 }
