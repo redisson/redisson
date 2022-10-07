fcefed302caf9c6ed925b150d1e85994af11868e
Nikita Koksharov <nkoksharov@redisson.pro>
2022-10-07 09:38:28 +0300

    Fixed - Redis Cluster topology scanned partially in case of DNS resolution error. #4581

--- redisson/src/main/java/org/redisson/cluster/ClusterConnectionManager.java
+++ redisson/src/main/java/org/redisson/cluster/ClusterConnectionManager.java
@@ -844,7 +844,7 @@ public class ClusterConnectionManager extends MasterSlaveConnectionManager {
         }
 
         CompletableFuture<Void> future = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
-        return future.handle((r, e) -> {
+        return future.thenApply(r -> {
             addCascadeSlaves(partitions.values());
 
             List<ClusterPartition> ps = partitions.values()
