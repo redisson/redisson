874602b38ebf9b4e834abaa45df1847f2c09d332
hgs <hgsssss@zuminternet.com>
2022-11-06 22:27:07 +0900

    Added Redis Mac OS Binary Path
    Signed-off-by: hgs <hgsssss@zuminternet.com>

--- redisson/src/test/java/org/redisson/RedissonRuntimeEnvironment.java
+++ redisson/src/test/java/org/redisson/RedissonRuntimeEnvironment.java
@@ -9,13 +9,22 @@ import java.util.Locale;
 public class RedissonRuntimeEnvironment {
 
     public static final boolean isTravis = "true".equalsIgnoreCase(System.getProperty("travisEnv"));
-    public static final String redisBinaryPath = System.getProperty("redisBinary", "C:\\redis\\redis-server.exe");
+    public static final String redisBinaryPath = System.getProperty("redisBinary", installPathByOS());
     public static final String tempDir = System.getProperty("java.io.tmpdir");
     public static final String OS;
     public static final boolean isWindows;
+    private static final String MAC_PATH = "/usr/local/opt/redis/bin/redis-server";
+    private static final String WINDOW_PATH = "C:\\redis\\redis-server.exe";
 
     static {
         OS = System.getProperty("os.name", "generic");
         isWindows = OS.toLowerCase(Locale.ENGLISH).contains("win");
+    }
+
+    private static String installPathByOS(){
+        final String OS = System.getProperty("os.name", "generic");
+        final boolean isMacOS = OS.toLowerCase(Locale.ENGLISH).contains("mac");
+
+        return isMacOS ? MAC_PATH : WINDOW_PATH;
     }
 }
