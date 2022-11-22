1de67f7388b359faaee0a453a4a8908774f3efec
Nikita Koksharov <nkoksharov@redisson.pro>
2022-11-22 14:29:13 +0300

    tests fixed

--- redisson/src/test/java/org/redisson/spring/session/TestServlet.java
+++ redisson/src/test/java/org/redisson/spring/session/TestServlet.java
@@ -1,13 +1,13 @@
 package org.redisson.spring.session;
 
-import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 @WebServlet(name = "/testServlet", urlPatterns = "/*")
 public class TestServlet extends HttpServlet {
