6dc2002451470924649f300a49f8a62520407eea
Nikita Koksharov <nkoksharov@redisson.pro>
2023-03-15 11:54:22 +0300

    Feature - CommandsLoadBalancer added

---
+++ redisson/src/main/java/org/redisson/connection/balancer/CommandsLoadBalancer.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2013-2022 Nikita Koksharov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.redisson.connection.balancer;
+
+import org.redisson.client.protocol.RedisCommand;
+import org.redisson.connection.ClientConnectionsEntry;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author Nikita Koksharov
+ *
+ */
+public class CommandsLoadBalancer extends RoundRobinLoadBalancer implements LoadBalancer {
+
+    private Set<String> commands;
+    private String hostName;
+
+    @Override
+    public ClientConnectionsEntry getEntry(List<ClientConnectionsEntry> clientsCopy, RedisCommand<?> redisCommand) {
+        if (commands.contains(redisCommand.getName().toLowerCase())) {
+            return clientsCopy.stream()
+                                .filter(c -> c.getClient().getAddr().getHostName().equals(hostName))
+                                .findAny()
+                                .orElseGet(() -> {
+                return getEntry(clientsCopy);
+            });
+        }
+        return getEntry(clientsCopy);
+    }
+
+    public void setHostName(String hostName) {
+        this.hostName = hostName;
+    }
+
+    public void setCommands(List<String> commands) {
+        this.commands = commands.stream()
+                                    .map(c -> c.toLowerCase())
+                                    .collect(Collectors.toSet());
+    }
+}
