b88567a188f1ee69789ec8c050f17726ef47a882
Nikita Koksharov <nkoksharov@redisson.pro>
2023-11-16 11:57:37 +0300

    refactoring

--- redisson/src/test/java/org/redisson/RedissonBloomFilterTest.java
+++ redisson/src/test/java/org/redisson/RedissonBloomFilterTest.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class RedissonBloomFilterTest extends BaseTest {
+public class RedissonBloomFilterTest extends RedisDockerTest {
 
     @Test
     public void testContainsAll() {
