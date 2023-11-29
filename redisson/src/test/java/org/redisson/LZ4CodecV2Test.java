5b580366bd683cc5eff14f6366e41902ea0b8035
Nikita Koksharov <nkoksharov@redisson.pro>
2023-11-29 13:56:55 +0300

    refactoring

--- redisson/src/test/java/org/redisson/LZ4CodecV2Test.java
+++ redisson/src/test/java/org/redisson/LZ4CodecV2Test.java
@@ -9,22 +9,18 @@ import org.redisson.config.Config;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class LZ4CodecV2Test extends BaseTest {
+public class LZ4CodecV2Test extends RedisDockerTest {
 
     @Test
     public void test1() {
-        Config config = new Config();
+        Config config = createConfig();
         config.setCodec(new LZ4Codec());
-        config.useSingleServer()
-                .setAddress(RedisRunner.getDefaultRedisServerBindAddressAndPort());
         RedissonClient r = Redisson.create(config);
         RBucket<String> s = r.getBucket("test1");
         s.set("12324");
 
-        Config config2 = new Config();
+        Config config2 = createConfig();
         config2.setCodec(new LZ4CodecV2());
-        config2.useSingleServer()
-                .setAddress(RedisRunner.getDefaultRedisServerBindAddressAndPort());
         RedissonClient r2 = Redisson.create(config2);
         RBucket<String> s2 = r2.getBucket("test1");
         assertThat(s2.get()).isEqualTo("12324");
