a488f745ccb282c3645651829e28b7c36b078116
Nikita Koksharov <nkoksharov@redisson.pro>
2024-07-02 11:07:16 +0300

    Fixed - RSearch AggregationOptions#loadAll() method doesn't work #6009

--- redisson/src/main/java/org/redisson/api/search/aggregate/AggregationOptions.java
+++ redisson/src/main/java/org/redisson/api/search/aggregate/AggregationOptions.java
@@ -68,7 +68,7 @@ public final class AggregationOptions {
     }
 
     public AggregationOptions loadAll() {
-        this.loadAll = loadAll;
+        this.loadAll = true;
         return this;
     }
 
