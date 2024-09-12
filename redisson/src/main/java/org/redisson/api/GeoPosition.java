09596c1e4cc6681aaba1c76bebb5a0db53714325
Nikita Koksharov <nkoksharov@redisson.pro>
2024-09-12 17:50:17 +0300

    refactoring

--- redisson/src/main/java/org/redisson/api/GeoPosition.java
+++ redisson/src/main/java/org/redisson/api/GeoPosition.java
@@ -15,6 +15,8 @@
  */
 package org.redisson.api;
 
+import java.util.Objects;
+
 /**
  * 
  * @author Nikita Koksharov
@@ -40,31 +42,16 @@ public class GeoPosition {
     }
 
     @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        long temp;
-        temp = Double.doubleToLongBits(latitude);
-        result = prime * result + (int) (temp ^ (temp >>> 32));
-        temp = Double.doubleToLongBits(longitude);
-        result = prime * result + (int) (temp ^ (temp >>> 32));
-        return result;
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GeoPosition that = (GeoPosition) o;
+        return Double.compare(longitude, that.longitude) == 0 && Double.compare(latitude, that.latitude) == 0;
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        GeoPosition other = (GeoPosition) obj;
-        if (Double.doubleToLongBits(latitude) != Double.doubleToLongBits(other.latitude))
-            return false;
-        if (Double.doubleToLongBits(longitude) != Double.doubleToLongBits(other.longitude))
-            return false;
-        return true;
+    public int hashCode() {
+        return Objects.hash(longitude, latitude);
     }
 
     @Override
