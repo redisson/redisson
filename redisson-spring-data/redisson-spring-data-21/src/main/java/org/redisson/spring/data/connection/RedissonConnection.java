7751f1b4968a2ab13e7709edb0ef71f47e190a61
Nikita Koksharov <nkoksharov@redisson.pro>
2024-12-24 13:54:24 +0300

    refactoring

--- redisson-spring-data/redisson-spring-data-21/src/main/java/org/redisson/spring/data/connection/RedissonConnection.java
+++ redisson-spring-data/redisson-spring-data-21/src/main/java/org/redisson/spring/data/connection/RedissonConnection.java
@@ -29,7 +29,7 @@ import org.redisson.client.protocol.RedisStrictCommand;
 import org.redisson.client.protocol.convertor.*;
 import org.redisson.client.protocol.decoder.*;
 import org.redisson.command.BatchPromise;
-import org.redisson.command.CommandAsyncService;
+import org.redisson.command.CommandAsyncExecutor;
 import org.redisson.command.CommandBatchService;
 import org.redisson.connection.MasterSlaveEntry;
 import org.redisson.misc.CompletableFutureWrapper;
@@ -67,14 +67,14 @@ public class RedissonConnection extends AbstractRedisConnection {
 
     private boolean closed;
     protected final Redisson redisson;
-    
-    CommandAsyncService executorService;
+
+    CommandAsyncExecutor executorService;
     private RedissonSubscription subscription;
     
     public RedissonConnection(RedissonClient redisson) {
         super();
         this.redisson = (Redisson) redisson;
-        executorService = (CommandAsyncService) this.redisson.getCommandExecutor();
+        executorService = this.redisson.getCommandExecutor();
     }
 
     @Override
@@ -1563,7 +1563,7 @@ public class RedissonConnection extends AbstractRedisConnection {
     }
 
     protected void resetConnection() {
-        executorService = (CommandAsyncService) this.redisson.getCommandExecutor();
+        executorService = this.redisson.getCommandExecutor();
         index = -1;
         indexToRemove.clear();
     }
