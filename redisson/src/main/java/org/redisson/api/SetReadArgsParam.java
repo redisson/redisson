b3e77c9eeb2439b5c8bf1420a8fcb59df989ffdb
seakider <seakider@gmail.com>
2025-05-19 00:51:57 +0800

    Feature - SocreSortedSet diff/union/intersect:add readIntersection() method #6562
    Signed-off-by: seakider <seakider@gmail.com>

--- redisson/src/main/java/org/redisson/api/SetReadArgsParam.java
+++ redisson/src/main/java/org/redisson/api/SetReadArgsParam.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (c) 2013-2024 Nikita Koksharov
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,7 +25,7 @@ class SetReadArgsParam implements SetReadArgs {
     private RScoredSortedSet.Aggregate aggregate;
     private Double[] weights;
 
-    public SetReadArgsParam(String[] names) {
+    SetReadArgsParam(String[] names) {
         this.names = names;
         this.aggregate = RScoredSortedSet.Aggregate.SUM;
     }
