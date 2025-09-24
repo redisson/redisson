2ddc4a4323889d6d2e4635fa3d902c5e192f1b00
mrniko <1104661+mrniko@users.noreply.github.com>
2025-09-24 08:29:19 +0300

    permits leak in AsyncSemaphore

--- redisson/src/main/java/org/redisson/misc/AsyncSemaphore.java
+++ redisson/src/main/java/org/redisson/misc/AsyncSemaphore.java
@@ -97,6 +97,8 @@ public final class AsyncSemaphore {
                 }
                 if (complete) {
                     return;
+                } else {
+                    counter.incrementAndGet();
                 }
             }
 
