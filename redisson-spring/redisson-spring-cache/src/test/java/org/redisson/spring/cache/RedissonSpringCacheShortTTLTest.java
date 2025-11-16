274c9bf162c38137506e977c15bf63a64abdb576
seakider <seakider@gmail.com>
2025-11-16 22:47:13 +0800

    improvement - Create redisson-spring-cache sub-module #6804
    Signed-off-by: seakider <seakider@gmail.com>

--- redisson-spring/redisson-spring-cache/src/test/java/org/redisson/spring/cache/RedissonSpringCacheShortTTLTest.java
+++ redisson-spring/redisson-spring-cache/src/test/java/org/redisson/spring/cache/RedissonSpringCacheShortTTLTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.redisson.RedisDockerTest;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.api.map.event.EntryEvent;
