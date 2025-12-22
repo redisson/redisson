936ffada925b395a6f838325e83ea1c516dc7d76
mrniko <1104661+mrniko@users.noreply.github.com>
2025-12-22 12:50:42 +0300

    Improvement - redisson-spring-boot-starter is a sub-module of redisson-spring #6806

---
+++ redisson-spring/redisson-spring-cache/src/main/java/org/redisson/spring/cache/RedissonCacheMeterBinderProviderV4.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2013-2024 Nikita Koksharov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.redisson.spring.cache;
+
+import io.micrometer.core.instrument.Tag;
+import io.micrometer.core.instrument.binder.MeterBinder;
+import org.springframework.boot.cache.metrics.CacheMeterBinderProvider;
+
+/**
+ * 
+ * 
+ * @author Nikita Koksharov
+ *
+ */
+public class RedissonCacheMeterBinderProviderV4 implements CacheMeterBinderProvider<RedissonCache> {
+
+    @Override
+    public MeterBinder getMeterBinder(RedissonCache cache, Iterable<Tag> tags) {
+        return new RedissonCacheMetrics(cache, tags);
+    }
+    
+}
---
+++ redisson-spring/redisson-spring-cache/src/main/java/org/redisson/spring/cache/RedissonCacheStatisticsAutoConfigurationV4.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2013-2024 Nikita Koksharov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.redisson.spring.cache;
+
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.cache.autoconfigure.CacheAutoConfiguration;
+import org.springframework.boot.cache.metrics.CacheMeterBinderProvider;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author Craig Andrews
+ * @author Nikita Koksharov
+ *
+ * {@link EnableAutoConfiguration Auto-configuration} for {@link RedissonCacheMeterBinderProvider}
+ *
+ */
+@Configuration
+@AutoConfigureAfter(CacheAutoConfiguration.class)
+@ConditionalOnBean(CacheManager.class)
+@ConditionalOnClass({CacheMeterBinderProvider.class, RedissonCache.class})
+public class RedissonCacheStatisticsAutoConfigurationV4 {
+    
+    @Bean
+    public RedissonCacheMeterBinderProviderV4 redissonCacheMeterBinderProvider(){
+        return new RedissonCacheMeterBinderProviderV4();
+    }
+    
+}
