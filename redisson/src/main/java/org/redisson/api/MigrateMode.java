b1afa4c20fd7f2037aac35d7510cbfe47d4c1398
mrniko <1104661+mrniko@users.noreply.github.com>
2026-02-12 18:35:48 +0300

    license header updated

--- redisson/src/main/java/org/redisson/api/MigrateMode.java
+++ redisson/src/main/java/org/redisson/api/MigrateMode.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2013-2024 Nikita Koksharov
+ * Copyright (c) 2013-2026 Nikita Koksharov
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.redisson.api;
 
 
