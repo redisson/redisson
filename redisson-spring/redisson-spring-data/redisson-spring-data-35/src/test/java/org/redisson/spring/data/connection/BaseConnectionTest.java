5fe55ecc35fb66cdc04a2b8e8938727ee6216428
mrniko <1104661+mrniko@users.noreply.github.com>
2026-03-05 12:58:08 +0300

    Spring Data 3.5 tests migration to Junit 5

--- redisson-spring/redisson-spring-data/redisson-spring-data-35/src/test/java/org/redisson/spring/data/connection/BaseConnectionTest.java
+++ redisson-spring/redisson-spring-data/redisson-spring-data-35/src/test/java/org/redisson/spring/data/connection/BaseConnectionTest.java
@@ -1,17 +1,17 @@
 package org.redisson.spring.data.connection;
 
-import org.junit.Before;
-import org.redisson.BaseTest;
+import org.junit.jupiter.api.BeforeAll;
+import org.redisson.RedisDockerTest;
 import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
-public abstract class BaseConnectionTest extends BaseTest {
+public abstract class BaseConnectionTest extends RedisDockerTest {
 
-    RedisConnection connection;
-    StringRedisTemplate redisTemplate;
+    static RedisConnection connection;
+    static StringRedisTemplate redisTemplate;
     
-    @Before
-    public void init() {
+    @BeforeAll
+    public static void init() {
         connection = new RedissonConnection(redisson);
         redisTemplate = new StringRedisTemplate();
         redisTemplate.setConnectionFactory(new RedissonConnectionFactory(redisson));
