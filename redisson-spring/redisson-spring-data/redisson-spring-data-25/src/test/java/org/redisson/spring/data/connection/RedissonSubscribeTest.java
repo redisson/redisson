d262db45e5c4c95ebe93fe5e74234a8a353a205a
mrniko <1104661+mrniko@users.noreply.github.com>
2026-03-06 09:26:08 +0300

    tests migration to JUnit 5

--- redisson-spring/redisson-spring-data/redisson-spring-data-25/src/test/java/org/redisson/spring/data/connection/RedissonSubscribeTest.java
+++ redisson-spring/redisson-spring-data/redisson-spring-data-25/src/test/java/org/redisson/spring/data/connection/RedissonSubscribeTest.java
@@ -1,76 +1,55 @@
 package org.redisson.spring.data.connection;
 
+import org.awaitility.Awaitility;
+import org.awaitility.Durations;
+import org.junit.jupiter.api.Test;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
-import org.assertj.core.api.Assertions;
-import org.awaitility.Awaitility;
-import org.awaitility.Durations;
-import org.junit.Test;
-import org.redisson.RedisRunner;
-import org.redisson.Redisson;
-import org.redisson.api.RedissonClient;
-import org.redisson.config.Config;
-import org.springframework.data.redis.connection.Message;
-import org.springframework.data.redis.connection.MessageListener;
-import org.springframework.data.redis.connection.RedisConnection;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.ChannelTopic;
-import org.springframework.data.redis.listener.PatternTopic;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class RedissonSubscribeTest extends BaseConnectionTest {
 
     @Test
     public void testPatterTopic() throws IOException, InterruptedException {
-        RedisRunner.RedisProcess instance = new RedisRunner()
-                .nosave()
-                .randomPort()
-                .randomDir()
-                .notifyKeyspaceEvents(
-                        RedisRunner.KEYSPACE_EVENTS_OPTIONS.K,
-                                    RedisRunner.KEYSPACE_EVENTS_OPTIONS.g,
-                                    RedisRunner.KEYSPACE_EVENTS_OPTIONS.E,
-                                    RedisRunner.KEYSPACE_EVENTS_OPTIONS.$)
-                .run();
+        testWithParams(redisson -> {
+            RedissonConnectionFactory factory = new RedissonConnectionFactory(redisson);
 
-        Config config = new Config();
-        config.useSingleServer().setAddress(instance.getRedisServerAddressAndPort()).setPingConnectionInterval(0);
-        RedissonClient redisson = Redisson.create(config);
+            RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+            container.setConnectionFactory(factory);
+            AtomicInteger counterTest = new AtomicInteger();
+            container.addMessageListener(new MessageListener() {
+                @Override
+                public void onMessage(Message message, byte[] pattern) {
+                    counterTest.incrementAndGet();
+                }
+            }, new PatternTopic("__keyspace@0__:mykey"));
+            container.addMessageListener(new MessageListener() {
+                @Override
+                public void onMessage(Message message, byte[] pattern) {
+                    counterTest.incrementAndGet();
+                }
+            }, new PatternTopic("__keyevent@0__:del"));
+            container.afterPropertiesSet();
+            container.start();
+            assertThat(container.isRunning()).isTrue();
 
-        RedissonConnectionFactory factory = new RedissonConnectionFactory(redisson);
+            RedisConnection c = factory.getConnection();
+            c.set("mykey".getBytes(), "2".getBytes());
+            c.del("mykey".getBytes());
 
-        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
-        container.setConnectionFactory(factory);
-        AtomicInteger counterTest = new AtomicInteger();
-        container.addMessageListener(new MessageListener() {
-            @Override
-            public void onMessage(Message message, byte[] pattern) {
-                counterTest.incrementAndGet();
-            }
-        }, new PatternTopic("__keyspace@0__:mykey"));
-        container.addMessageListener(new MessageListener() {
-            @Override
-            public void onMessage(Message message, byte[] pattern) {
-                counterTest.incrementAndGet();
-            }
-        }, new PatternTopic("__keyevent@0__:del"));
-        container.afterPropertiesSet();
-        container.start();
-        Assertions.assertThat(container.isRunning()).isTrue();
-
-        RedisConnection c = factory.getConnection();
-        c.set("mykey".getBytes(), "2".getBytes());
-        c.del("mykey".getBytes());
-
-        Awaitility.await().atMost(Durations.FIVE_SECONDS).until(() -> {
-            return counterTest.get() == 3;
-        });
-
-        container.stop();
-        redisson.shutdown();
+            Awaitility.await().atMost(Durations.FIVE_SECONDS).until(() -> {
+                return counterTest.get() == 3;
+            });
+        }, NOTIFY_KEYSPACE_EVENTS, "KgE$");
     }
 
     @Test
